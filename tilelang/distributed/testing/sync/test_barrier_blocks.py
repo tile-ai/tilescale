8a0033bd2b6dae04eb36bf9c7652f533d6cfab8a
Rachmanino <18805904201@163.com>
2025-08-20 07:16:56 +0000

    Migrate to `sync.h` and fix the absence of barrier for producers in warp-specialized codegen.

--- tilelang/distributed/testing/sync/test_barrier_blocks.py
+++ tilelang/distributed/testing/sync/test_barrier_blocks.py
@@ -1,8 +1,10 @@
 import tilelang
 import tilelang.language as T
+from tilelang.carver.arch import driver
 import torch
+import argparse
 
-# tilelang.disable_cache()
+tilelang.disable_cache()
 
 
 @tilelang.jit(out_idx=-1)
@@ -24,16 +26,30 @@ def get_test_barrier_blocks_kernel(num_blocks: int, threads: int):
     return main
 
 
-def test_barrier_blocks(num_blocks: int = 256, threads: int = 128):
+def test_barrier_blocks(num_blocks: int = 64, threads: int = 128, print_source: bool = False):
     kernel = get_test_barrier_blocks_kernel(num_blocks, threads)
     bar = torch.tensor([0], dtype=torch.int32, device='cuda')
     input = torch.zeros(threads, dtype=torch.int32, device='cuda')
+    if print_source:
+        print(kernel.get_kernel_source())
+    print('Compilation done, start running...')
 
-    print('Compilation done..')
     output = kernel(input, bar)
+
     assert torch.all(output == num_blocks)
     print('Check passed✅')
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--blocks', type=int, default=64)
+    parser.add_argument('--threads', type=int, default=128)
+    parser.add_argument('--print_source', action='store_true')
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    test_barrier_blocks()
+    args = parse_args()
+    assert args.blocks <= driver.get_num_sms(
+    ), f'Launched {args.blocks} blocks, which is larger than the number of SM ({driver.get_num_sms()}) on the current device and may cause deadlock!'
+    test_barrier_blocks(args.blocks, args.threads, args.print_source)
