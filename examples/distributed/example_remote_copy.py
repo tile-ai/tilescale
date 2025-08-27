c3d0298ff5a56aedcf2c1ecb1bbdee07e186089f
Rachmanino <18805904201@163.com>
2025-08-27 03:18:52 +0000

    [Bugfix] Fix the ipc bug caused by incorrect tensor management and update example_remote_copy
    - Updated `utils.py` to include a new `create_tensor` function for explicit CUDA memory allocation, enhancing IPC compatibility.
    
    - Update example_remote_copy

--- examples/distributed/example_remote_copy.py
+++ examples/distributed/example_remote_copy.py
@@ -4,12 +4,12 @@ import argparse
 import torch
 import torch.distributed as dist
 import torch.multiprocessing
-from tilelang.distributed.utils import init_dist, create_dist_tensor
+from tilelang.distributed.utils import init_dist, create_dist_tensor, create_tensor
 
 tilelang.disable_cache()
 
 
-def kernel_(M, N, num_rank, block_M, block_N, threads):
+def kernel_(M, N, num_rank, block_M, threads):
 
     @T.prim_func
     def main(
@@ -18,11 +18,12 @@ def kernel_(M, N, num_rank, block_M, block_N, threads):
             rank: T.Tensor((1), "int32"),
             meta_data: T.Tensor((1, num_rank), "uint64", buffer_type="meta_data"),
     ):
-        with T.Kernel(T.ceildiv(M, block_M), T.ceildiv(N, block_N), threads=threads) as (bx, by):
+        with T.Kernel(T.ceildiv(M, block_M), threads=threads) as bid:
+            #TODO(ipc): Add work partition among warps and vectorized copy
             T.remote_copy(
-                src=T.address_of(src[0, 0]),
-                dst=T.address_of(dst[0, 0]),
-                size=M * N,
+                src=T.address_of(src[bid * block_M, 0]),
+                dst=T.address_of(dst[bid * block_M, 0]),
+                size=block_M * N,
                 dst_pe=rank[0] ^ 1,
                 unroll_factor=4)
 
@@ -32,18 +33,17 @@ def kernel_(M, N, num_rank, block_M, block_N, threads):
 def main(local_rank: int, num_local_ranks: int, args: argparse.Namespace):
     M, N = args.M, args.N
     BLOCK_M = 128
-    BLOCK_N = 256
-    threads = 256
+    threads = 128
 
     rank, num_ranks, group = init_dist(local_rank, num_local_ranks)
     src = torch.randn(M, N, device="cuda", dtype=torch.float32)
-    dst = torch.empty(M, N, device="cuda", dtype=torch.float32)
+    dst = create_tensor([M, N], torch.float32)
     rank_tensor = torch.tensor([local_rank], device="cuda", dtype=torch.int32)
     buffer_ptrs_gpu = create_dist_tensor(local_rank, num_local_ranks, dst, rank,
                                          group).reshape(1, num_local_ranks)
 
-    kernel = tilelang.compile(kernel_(M, N, num_ranks, BLOCK_M, BLOCK_N, threads))
-    if local_rank == 0:
+    kernel = tilelang.compile(kernel_(M, N, num_ranks, BLOCK_M, threads))
+    if local_rank == 0 and args.print_source:
         print(kernel.get_kernel_source())
 
     torch.cuda.synchronize()
@@ -70,8 +70,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--num-processes', type=int, default=2, help='Number of processes to spawn (default: 2)')
-    parser.add_argument('--M', type=int, default=8192, help='M dimension')
-    parser.add_argument('--N', type=int, default=8192, help='N dimension')
+    parser.add_argument('--M', type=int, default=256, help='M dimension')
+    parser.add_argument('--N', type=int, default=256, help='N dimension')
+    parser.add_argument(
+        '--print-source', action='store_true', help='Print the source code of the kernel')
     args = parser.parse_args()
     num_processes = args.num_processes
+
     torch.multiprocessing.spawn(main, args=(num_processes, args), nprocs=num_processes)
