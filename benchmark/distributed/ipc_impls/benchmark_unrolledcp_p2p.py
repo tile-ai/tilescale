3ec4d26ac9cebb1afb903af57b80af0646b6244f
Rachmanino <18805904201@163.com>
2025-09-05 10:09:50 +0000

    fix typo and accept `unroll_factor` as an argument

--- benchmark/distributed/ipc_impls/benchmark_unrolledcp_p2p.py
+++ benchmark/distributed/ipc_impls/benchmark_unrolledcp_p2p.py
@@ -13,7 +13,7 @@ tilelang.disable_cache()
 os.environ['NCCL_DEBUG'] = 'WARN'
 
 
-def ipc_kernel_push(size, threads):
+def ipc_kernel_push(size, threads, unroll_factor):
 
     @T.prim_func
     def ipc_push(
@@ -31,12 +31,12 @@ def ipc_kernel_push(size, threads):
                 dst=T.address_of(dst[warp_start]),
                 size=warp_copy_size,
                 dst_pe=rank[0] ^ 1,
-                unroll_factor=4)
+                unroll_factor=unroll_factor)
 
     return ipc_push
 
 
-def ipc_kernel_pull(size, threads) :
+def ipc_kernel_pull(size, threads, unroll_factor) :
 
     @T.prim_func
     def ipc_pull(
@@ -54,7 +54,7 @@ def ipc_kernel_pull(size, threads) :
                 dst=T.address_of(dst[warp_start]),
                 size=warp_copy_size,
                 src_pe=rank[0] ^ 1,
-                unroll_factor=4)
+                unroll_factor=unroll_factor)
 
     return ipc_pull
 
@@ -63,7 +63,7 @@ def benchmark_ipc_bw(rank: int, num_ranks: int, group: dist.ProcessGroup, size: 
     assert num_ranks == 2, "this benchmark only supports 2 ranks"
     assert args.threads % 32 == 0, "threads must be divisible by 32"
 
-    kernel = tilelang.compile(ipc_kernel_push(size, args.threads))
+    kernel = tilelang.compile(ipc_kernel_push(size, args.threads, args.unroll_factor))
     kernel.initialize(allocator=allocator)
     src = tilelang.tensor((size,), torch.float32, allocator=allocator).random_()
     dst = tilelang.tensor((size,), torch.float32, allocator=allocator)
@@ -74,12 +74,12 @@ def benchmark_ipc_bw(rank: int, num_ranks: int, group: dist.ProcessGroup, size: 
     dist.barrier(group)
     torch.cuda.synchronize()
     _, t_push = perf_fn(push_fn, args.warmup, args.repeat)  # 1st returned value is output
-    bw_push = (size* 1e-9) / (t_push * 1e-3)
+    bw_push = (size*4* 1e-9) / (t_push * 1e-3)
 
     dist.barrier(group)
 
     # Re-use allocator and tensors
-    kernel = tilelang.compile(ipc_kernel_pull(size, args.threads))
+    kernel = tilelang.compile(ipc_kernel_pull(size, args.threads, args.unroll_factor))
     kernel.initialize(allocator=allocator)
 
     def pull_fn():
@@ -88,7 +88,7 @@ def benchmark_ipc_bw(rank: int, num_ranks: int, group: dist.ProcessGroup, size: 
     dist.barrier(group)
     torch.cuda.synchronize()
     _, t_pull = perf_fn(pull_fn, args.warmup, args.repeat)  # 1st returned value is output
-    bw_pull = (size* 1e-9) / (t_pull * 1e-3)
+    bw_pull = (size*4* 1e-9) / (t_pull * 1e-3)
 
     dist.barrier(group)
 
@@ -99,7 +99,7 @@ def main(local_rank: int, num_local_ranks: int, args: argparse.Namespace):
     rank, num_ranks, group = init_dist(local_rank, num_local_ranks)
 
     allocator = tilelang.get_allocator(
-        size=2**35,
+        size=2**30,
         device="cuda",
         is_distributed=True,
         local_rank=rank,
@@ -120,6 +120,7 @@ if __name__ == "__main__":
     parser.add_argument("--warmup", type=int, default=10, help="number of warmup iterations (default: 10)")
     parser.add_argument("--repeat", type=int, default=50, help="number of repeat iterations (default: 10)")
     parser.add_argument("--threads", type=int, default=128, help="Threads per block (default: 128)")
+    parser.add_argument("--unroll-factor", type=int, default=4, help="Unroll factor (default: 4)")
     args = parser.parse_args()
     nprocs = 2
 
