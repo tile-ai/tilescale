b555cffaaff10e815edb94156e2cb571a5a7abde
Yu Cheng <yu.cheng@pku.edu.cn>
2025-09-26 20:31:58 +0800

    fix

--- tilelang/distributed/__init__.py
+++ tilelang/distributed/__init__.py
@@ -1,4 +1,4 @@
 """The distributed modules"""
 
-from .common.ipc_ext import _create_ipc_handle, _sync_ipc_handles, _create_tensor  # noqa: F401
+from .common import _create_ipc_handle, _sync_ipc_handles, _create_tensor  # noqa: F401
 from .utils import *  # noqa: F401
---
+++ tilelang/distributed/common/__init__.py
@@ -0,0 +1,3 @@
+"""The common distributed modules"""
+
+from .ipc_ext import _create_ipc_handle, _sync_ipc_handles, _create_tensor  # noqa: F401
